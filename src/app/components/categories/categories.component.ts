a7760a2eeb936e06c0306cd4eb1045c2f9cf7fa3
omniaaaaahmed <omnia.ahmed@kortobaa.com>
2022-11-23 16:52:54 +0200

    'get categories data and handel ui'

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Category } from 'src/app/interfaces/category';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent implements OnInit, OnDestroy {
   constructor(private __cateService: CategoriesService) {}
+  categories: Array<Category> = [];
   ngOnInit(): void {
-    this.__cateService.getAllCategories().subscribe((x) => {
-      console.log(x);
+    this.__cateService.getAllCategories().subscribe((cate) => {
+      this.categories = cate;
+      console.log(this.categories);
     });
   }
+  ngOnDestroy(): void {}
 }
--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from 'src/app/components/categories/categories.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [CategoriesComponent],
-  imports: [CommonModule, CategoriesRoutingModule],
+  imports: [CommonModule, CategoriesRoutingModule,MatCardModule],
   exports: [CategoriesComponent],
 })
 export class CategoriesModule {}
