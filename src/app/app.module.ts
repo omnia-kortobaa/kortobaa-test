59a473b5f48dca0b44d16b62036730998b6bb2c4
omniaaaaahmed <omnia.ahmed@kortobaa.com>
2022-11-23 11:28:03 +0200

    'create authors module'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ItemsComponent } from './components/items/items.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AuthorsComponent } from './components/authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemsComponent,
+    CategoriesComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
